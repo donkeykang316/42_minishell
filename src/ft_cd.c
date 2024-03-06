de3e5e033a7148961689a80e61b9cb72e5fcd80f
donkeykang316 <kang_an_an@hotmail.de>
2024-03-06 18:04:45 +0100

    ft_cd update

--- src/ft_cd.c
+++ src/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 11:00:18 by kaan              #+#    #+#             */
-/*   Updated: 2024/03/06 15:23:44 by kaan             ###   ########.fr       */
+/*   Updated: 2024/03/06 17:58:19 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,33 @@ static void	ft_chdir(char *str, char *pwd)
 void	ft_cd(char *str)
 {
 	char	*pwd;
+	char	*dpath;
 	int		len;
 
+	pwd = getcwd(NULL, 0);
 	if (ft_strnstr(str, "..", ft_strlen(str)))
 	{
-		pwd = getcwd(NULL, 0);
 		len = ft_strlen(pwd);
 		while (pwd[len] != 47)
 			len--;
 		pwd[len] = '\0';
 		ft_chdir(str, pwd);
-		free(pwd);
+	}
+	else if (!ft_memcmp(pwd, str, ft_strlen(pwd)))
+	{
+		dpath = ft_substr(str, ft_strlen(pwd), ft_strlen(str));
+		ft_chdir(str, dpath);
+		free(dpath);
 	}
 	else
 	{
 		len = 3;
 		while (str[len] == 32)
 			len++;
-		pwd = ft_substr(str, len, ft_strlen(str));
-		ft_chdir(str, pwd);
-		free(pwd);
+		dpath = ft_substr(str, len, ft_strlen(str));
+		ft_chdir(str, dpath);
+		free(dpath);
 	}
+	free(pwd);
 	free(str);
 }
