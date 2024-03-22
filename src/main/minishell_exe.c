eefeaadf0586a5d2cad85555fc2128549949d203
Kang An <kaan@c4c2c2.42berlin.de>
2024-03-22 18:39:29 +0100

    sigal need to be reworked

--- src/main/minishell_exe.c
+++ src/main/minishell_exe.c
@@ -6,17 +6,51 @@
 /*   By: kaan <kaan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 12:01:19 by kaan              #+#    #+#             */
-/*   Updated: 2024/03/22 15:17:32 by kaan             ###   ########.fr       */
+/*   Updated: 2024/03/22 18:35:16 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/exe.h"
+
+int	shell_reset(int nbr)
+{
+	return (nbr - 0);
+}
 
 void	handle_ctrlc(int sig)
 {
 	(void)sig;
 	printf("\n");
 	printf(CL_NAME);
+}
+
+void	handle_ctrld(int sig)
+{
+	(void)sig;
+	printf("exit");
+	exit(0);
+}
+
+void	shell_loop(t_prompt *prompt)
+{
+	int	nbr;
+
+	nbr = 0;
+	while (1)
+	{
+		//disable "ctrl-\"
+		/* if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
+			ft_exit("quit\n"); */
+		signal(SIGQUIT, handle_ctrld);
+		//enable "ctrl-c"
+		signal(SIGINT, handle_ctrlc);
+		prompt->line = readline(CL_NAME);
+		//enable "ctrl-d"
+		if (!prompt->line)
+			ft_exit("quit minishell");
+		split_tokens(prompt);
+		add_history(prompt->line);
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -32,18 +66,5 @@ int	main(int argc, char **argv, char **envp)
 	prompt->envp->env = double_dup(envp);
 	if (!prompt->envp->env)
 		simple_err(ERR_ENV);
-	while (1)
-	{
-		//disable "ctrl-\"
-		if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
-			ft_exit("quit\n");
-		//enable "ctrl-c"
-		signal(SIGINT, handle_ctrlc);
-		prompt->line = readline(CL_NAME);
-		//enable "ctrl-d"
-		if (!prompt->line)
-			ft_exit("quit minishell");
-		split_tokens(prompt);
-		add_history(prompt->line);
-	}
+	shell_loop(prompt);
 }
