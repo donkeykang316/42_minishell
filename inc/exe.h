906887ac1e5a9e189cbed9e5ad979df452a96ed3
donkeykang316 <kang_an_an@hotmail.de>
2024-04-14 06:01:51 +0200

    cd path complete(?)

--- inc/exe.h
+++ inc/exe.h
@@ -6,7 +6,7 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 13:01:29 by kaan              #+#    #+#             */
-/*   Updated: 2024/04/12 13:08:25 by kaan             ###   ########.fr       */
+/*   Updated: 2024/04/14 04:56:13 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 # define EXE_H
 
 # include "minishell.h"
+
+typedef struct	s_tmp
+{
+	char	*temp;
+	char	*temp1;
+	char	*path;
+	char	**all_path;
+	int	i;
+}		t_tmp;
 
 void	exe_main(t_prompt *prompt);
 void    ft_echo(t_prompt *prompt);
--- src/exe/cd_pwd.c
+++ src/exe/cd_pwd.c
@@ -6,7 +6,7 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 13:00:45 by kaan              #+#    #+#             */
-/*   Updated: 2024/04/12 13:31:21 by kaan             ###   ########.fr       */
+/*   Updated: 2024/04/14 06:00:15 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,66 @@ void	ft_pwd(t_prompt *prompt)
 	}
 }
 
+void    ft_free(char **ptr)
+{
+    int     i;
+
+    i = 0;
+    if (ptr)
+    {
+        while (ptr[i])
+        {
+            free(ptr[i]);
+            i++;
+        }
+    	free(ptr);
+    }
+}
+
+char    *get_env(char **env, char *env_var)
+{
+    int     i;
+
+    i = 0;
+    while (env[i])
+    {
+        if (!ft_strncmp(env_var, env[i], 4))
+        	    return (env[i]);
+        i++;
+    }
+    return (NULL);
+}
+
+char    *get_home(t_prompt *prompt)
+{
+    t_tmp   tmp;
+
+    tmp.i = 0;
+	tmp.all_path = ft_split(get_env(prompt->envp->env, "HOME"), 61);
+    while (prompt->envp->env[tmp.i])
+    {
+        tmp.path = ft_strdup(tmp.all_path[tmp.i]);
+		if (access(tmp.path, R_OK | X_OK) != -1)
+		{
+			ft_free(tmp.all_path);
+        	return (tmp.path);
+		}
+    	free(tmp.path);
+    	tmp.i++;
+    }
+	ft_free(tmp.all_path);
+	return (NULL);
+}
+
 void	ft_cd(t_prompt *prompt)
 {
+	char	*path;
 	if (!ft_memcmp_ms(prompt->parser->cmd, "cd"))
 	{
 		if (!prompt->parser->args[0])
 		{
-			printf("empty argument\n");
+			path = get_home(prompt);
+			chdir(path);
 			return ;
 		}
 		if (prompt->parser->args[1])
