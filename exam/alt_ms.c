5b92fa40ba59796c744819584cd3898c7fe36dcc
donkeykang316 <kang_an_an@hotmail.de>
2024-05-15 11:43:22 +0200

    exam complete

--- exam/alt_ms.c
+++ exam/alt_ms.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 
 int	err(char *str)
@@ -18,7 +18,7 @@ int	cd(char **av, int i)
 	return 0;
 }
 
-int    execute(char **av, int i, char **env)
+int	exec(char **av, char **envp, int i)
 {
 	int	fd[2];
 	int status;
@@ -33,27 +33,35 @@ int    execute(char **av, int i, char **env)
 		av[i] = 0;
 		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			return err("error: fatal\n");
-		execve(*av, av, env);
+		execve(*av, av, envp);
 		return err("error: cannot execute "), err(*av), err("\n");
 	}
 
 	waitpid(pid, &status, 0);
 	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 		return err("error: fatal\n");
+
+	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
-int	main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **envp)
 {
-    int i = 0;
+	int	i = 0;
+	int status = 0;
 
-    while (av[i] && av[i + 1])
-    {
-        av = &av[i + 1];
-        while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
-			    i++;
-        if (!strcmp(*av, "cd"))
-		    cd(av, i);
-        else if (i)
-            execute(av, i, env);
-    }
+	if (ac > 1)
+	{
+		while (av[i] && av[++i])
+		{
+			av += i;
+			i = 0;
+			while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+				i++;
+			if (!strcmp(*av, "cd"))
+				status = cd(av, i);
+			else if(i)
+				status = exec(av, envp, i);
+		}
+	}
+	return status;
 }
